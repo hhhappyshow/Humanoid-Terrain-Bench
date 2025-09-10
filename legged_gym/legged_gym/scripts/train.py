c2e69038ab180fd255e2a1302d40b398b261a16d
hhhappyshow <1253332472@qq.com>
2025-09-10 18:35:53 +0800

    w&b和重新更新obs

--- legged_gym/legged_gym/scripts/train.py
+++ legged_gym/legged_gym/scripts/train.py
@@ -54,7 +54,7 @@ def train(args):
     
     if args.no_wandb:
         mode = "disabled"
-    wandb.init(project=args.proj_name, name=args.exptid, entity="994153566-swjtu", group=args.exptid[:3], mode=mode, dir="../../logs")
+    wandb.init(project=args.proj_name, name=args.exptid, entity="hhhappyshow-institution", group=args.exptid[:3], mode=mode, dir="../../logs")
     wandb.save(LEGGED_GYM_ENVS_DIR + "/base/legged_robot_config.py", policy="now")
     wandb.save(LEGGED_GYM_ENVS_DIR + "/base/legged_robot.py", policy="now")
 
